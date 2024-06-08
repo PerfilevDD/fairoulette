4c4b20a061d9d3b4f4cde92ffb0b88802610f88f
perfilevd0 <s74dperf@uni-bonn.de>
2024-06-08 22:20:31 +0200

    feat/fastAPI

--- client/src/client.py
+++ client/src/client.py
@@ -1,17 +1,12 @@
 import requests
 
 def get_random_number():
-    url = "http://localhost:25565/get_random_number"
-    #payload = {'x': x, 'y': y}
-    #data = response.json()
-    response = requests.post(url, json={"e":"e"})
+    url = "http://localhost:8000"
+    response = requests.get(f"{url}/get_random_number")
     if response.ok:
         print(response.json())
-    #data = response.json()
-    #return data['result']
+        #print(response.json()['result'])
 
 if __name__ == "__main__":
     while(True):
-        #test_input = input()
         result = get_random_number()
-        #print(f"Res: {result}")
--- server/extra/server.py
+++ server/extra/server.py
@@ -1,14 +1,11 @@
-from flask import Flask, request, jsonify
-from fairoulette import Randomizer
+from fastapi import FastAPI
+from fairoulette import Randomizer # type: ignore
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/get_random_number', methods=['POST'])
-def get_random_number():
+@app.get('/get_random_number')
+async def get_random_number():
     randomizer = Randomizer()
     result = randomizer.get_random_number();
     print(f'{result}')
-    return jsonify({'result': result})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=25565)
+    return {'result': result}
